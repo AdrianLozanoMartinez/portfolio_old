74171304e546c1388b829990ea045cd8d0f911e9
Adrian Lozano <adrianlm1985@gmail.com>
2021-07-20 13:49:40 +0200

    Inicio de portfolio, navbar, foot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Routes
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
+
+//Material angular
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';    
+
+//Component
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FootComponent } from './component/foot/foot.component';
+import { HomeComponent } from './page/home/home.component';
+import { ErrorComponent } from './component/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FootComponent,
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //Routes
+    RouterModule.forRoot( ROUTES, { useHash: true } ),
+    //Material angular
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatGridListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FootComponent } from './component/foot/foot.component';
+import { HomeComponent } from './page/home/home.component';
+import { ErrorComponent } from './component/error/error.component';
+
+export  const ROUTES: Routes = [                    
+   { path: '', component: HomeComponent },                
+   { path: 'home', component: HomeComponent},   
+   { path: 'foot', component: FootComponent},   
+   { path: 'navbar', component: NavbarComponent},   
+   { path: 'appcomponente', component: AppComponent },
+//    { path: '**', component: ErrorComponent},   
+   { path: '**', pathMatch: 'full', redirectTo: 'home'},   
+];
